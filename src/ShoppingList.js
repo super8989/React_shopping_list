ce7f3100ec6e63c91fda9980e68fd04c7c442b35
Sam <super8989@gmail.com>
2019-10-20 12:27:34 -0400

    added in state and renderItems function to render the shopping list

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
+import { unlink } from "fs";
 
 class ShoppingList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [
+				{ name: "Milk", qty: "2 gallons" },
+				{ name: "Bread", qty: "2 loaves" }
+			]
+		};
+	}
+
+	renderItems() {
+		return (
+			<ul>
+				{this.state.items.map(item => (
+					<li>
+						{item.name}: {item.qty}
+					</li>
+				))}
+			</ul>
+		);
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Shopping List</h1>
+				{this.renderItems()}
 			</div>
 		);
 	}
