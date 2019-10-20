49f5b3151bc5c32b9306cd19774a9c4eaa17719e
Sam <super8989@gmail.com>
2019-10-20 13:27:24 -0400

    using the UUID library to add in unique ids as key to list items

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import uuid from "uuid/v4";
 
 class ShoppingList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			items: [
-				{ name: "Milk", qty: "2 gallons" },
-				{ name: "Bread", qty: "2 loaves" }
+				{ name: "Milk", qty: "2 gallons", id: uuid() },
+				{ name: "Bread", qty: "2 loaves", id: uuid() }
 			]
 		};
 		this.addItem = this.addItem.bind(this);
 	}
 
 	addItem(item) {
+		let newItem = { ...item, id: uuid() };
 		this.setState(state => ({
-			items: [...state.items, item]
+			items: [...state.items, newItem]
 		}));
 	}
 
@@ -23,7 +25,7 @@ class ShoppingList extends Component {
 		return (
 			<ul>
 				{this.state.items.map(item => (
-					<li>
+					<li key={item.id}>
 						{item.name}: {item.qty}
 					</li>
 				))}
